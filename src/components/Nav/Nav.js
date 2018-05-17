f4bdc5bb12285342a46f2e504b30f5be74b57392
Alan Curtis <fakeemail.@email.com>
2018-05-17 14:02:47 -0600

    added react scroll to component and updaed nav.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
+import scrollToComponent from 'react-scroll-to-component';
 
-function Nav (){    
+function Nav (props){ 
         return(
             <div className="Nav">
-                 Nav comp 
+                 <button className="contact" onClick={()=>props.handleScroll('Intro')}>Home</button>
+                 <button className="contact" onClick={()=>props.handleScroll('About')}>About</button>
+                 <button className="contact" onClick={()=>props.handleScroll('Portfolio')}>Portfolio</button>
+                 <button className="contact" onClick={()=>props.handleScroll('Contact')}>Contact</button>
             </div>
         )
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,16 +7,39 @@ import Nav from '../../components/Nav/Nav';
 import Intro from '../../components/Intro/Intro';
 import Portfolio from '../../components/Portfolio/Portfolio';
 
-class Home extends Component{    
+// libraries
+import scrollToComponent from 'react-scroll-to-component';
+
+class Home extends Component{ 
+    constructor(){
+        super();
+        this.handleScroll= this.handleScroll.bind(this)
+    }  
+
+    handleScroll(section){
+        console.log(section)
+        scrollToComponent(this[section], { offset: -60, align: 'top', duration: 1000,  ease:'inOutExpo'})
+    }
+
     render(){
         return(
             <div className="Home">
-                 <Intro/>
-                 <Nav/>
-                 <About/>
-                 <Portfolio/>
-                 <Contact/>
-                 <Footer/>
+                 <section ref={(section) => { this.Intro = section; }}>
+                    <Intro/>
+                 </section>
+                 <Nav handleScroll={this.handleScroll}/>
+                 <section ref={(section) => { this.About = section; }}>
+                    <About/>
+                 </section>
+                 <section ref={(section) => { this.Portfolio = section; }}>
+                    <Portfolio/>
+                 </section>
+                 <section ref={(section) => { this.Contact = section; }}>
+                    <Contact />
+                 </section>
+                 <section ref={(section) => { this.Footer = section; }}>
+                    <Footer/>  
+                 </section>
             </div>
         )
     }
