056e8b1db35a4c86225bfb888e001b257c5e7920
Alan Curtis <fakeemail.@email.com>
2018-06-06 21:18:32 -0600

    One timeline.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,57 +1,35 @@
 import React from 'react';
-import {TweenLite, TimelineMax} from 'gsap';
+import {TweenLite, TimelineMax, Power4, Back} from 'gsap';
 import Waypoint from 'react-waypoint';
-
 
 import Profile from '../Profile/Profile';
 import Talents from '../Talents/Talents';
 
-function About (){  
-        let titlePlayed = false; 
-        function animateTitle(){
-            console.log('animate title')
-            if (!titlePlayed){
-                const tl = new TimelineMax().delay(.5)
-                tl
-                .to('.title', 1, {opacity:1 , x:0 } )
-                titlePlayed = true
-            }
-        } 
+function About() {
+    function animate() {
+        console.log('animate title')
+        const tl =  new TimelineMax().delay(.5)
+        tl
+        .to('.title', .75, {ease: Power4.easeOut, opacity:1 , x:0 } )
+        .staggerTo('.Talents div', .75, { ease: Power4.easeOut, opacity:1}, .25 )
+        .staggerTo('.Talents div svg', .75, { ease: Power4.easeOut, rotationY: 0}, .25, '-=.8')
+        .to('.Profile', .75, { ease: Power4.easeOut, opacity: 1, y:0} )
+        .to('.Profile .container img', .75, { ease: Back.easeOut, opacity: 1, scale:1} )
+    }
 
-        function animateTalents(){
-            console.log('animate Talents')
-            const tl = new TimelineMax().delay(.5)
-            tl
-            .staggerTo('.Talents div', 1, {opacity:1}, .5 )
-            .staggerTo('.Talents div svg', 1, {rotationY: 0}, .5, '-=1.75')
-        } 
-        function animateProfile(){
-            console.log('animate profile')
-            const tl = new TimelineMax().delay(.5)
-            tl
-            .to('.Profile', 1, {opacity: 1, x:0} )
-        } 
-        return(
-            <div className="About">
+    return (
+        <div className="About">
+            <Waypoint onEnter={() => animate()} bottomOffset='30%'>
                 <div className='container'>
                     <div className='grid'>
-                        <Waypoint onEnter={()=>animateTitle()} bottomOffset='0%' >
-                            <h1 className='title'>About</h1>
-                        </Waypoint>
-                        <Waypoint onEnter={()=>animateTalents()} bottomOffset='50%' >
-                            <div>
-                                <Talents/>
-                            </div>
-                        </Waypoint>
-                        <Waypoint onEnter={()=>animateProfile()} bottomOffset='50%' >
-                            <div>
-                                <Profile/> 
-                            </div>
-                        </Waypoint>
+                        <h1 className='title'>About</h1>
+                        <Talents/>
+                        <Profile/>
                     </div>
                 </div>
-            </div>
-        )
+            </Waypoint>
+        </div>
+    )
 }
 
 export default About;
