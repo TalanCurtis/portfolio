9582598e7b301cb92025c673f9d03f0f83eda5ce
Alan Curtis <fakeemail.@email.com>
2018-06-07 11:59:51 -0600

    added svg line and update animation on about intro.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,17 +4,19 @@ import Waypoint from 'react-waypoint';
 
 import Profile from '../Profile/Profile';
 import Talents from '../Talents/Talents';
+import LineSvg from '../../images/LineSvg';
 
 function About() {
     function animate() {
         console.log('animate title')
         const tl =  new TimelineMax().delay(.5)
         tl
-        .to('.title', .75, {ease: Power4.easeOut, opacity:1 , x:0 } )
-        .staggerTo('.Talents div', .75, { ease: Power4.easeOut, opacity:1}, .25 )
+        .to('.sectionTitle .title', .75, {ease: Power4.easeOut, opacity:1 , x:0 } )
+        .to('.sectionTitle svg', .75, {ease: Power4.easeOut, opacity:1 , x:0 }, '-=.45' )
+        .staggerTo('.Talents div', .75, { ease: Power4.easeOut, opacity:1}, .25 , '-=.45')
         .staggerTo('.Talents div svg', .75, { ease: Power4.easeOut, rotationY: 0}, .25, '-=.8')
-        .to('.Profile', .75, { ease: Power4.easeOut, opacity: 1, y:0} )
-        .to('.Profile .container img', .75, { ease: Back.easeOut, opacity: 1, scale:1} )
+        .to('.Profile', .75, { ease: Power4.easeOut, opacity: 1, y:0}, '-=.45')
+        .to('.Profile .container img', .75, { ease: Back.easeOut, opacity: 1, scale:1}, '-=.45' )
     }
 
     return (
@@ -22,7 +24,10 @@ function About() {
             <Waypoint onEnter={() => animate()} bottomOffset='30%'>
                 <div className='container'>
                     <div className='grid'>
-                        <h1 className='title'>About</h1>
+                        <div className='sectionTitle'>
+                            <h1 className='title'>About</h1>
+                            <LineSvg width='100'/>
+                        </div>
                         <Talents/>
                         <Profile/>
                     </div>
